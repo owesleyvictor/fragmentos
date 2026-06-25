bf98c91af520e23fb331c850ac69900800983ead
owesleyvictor <wesleyvictor2013.wvp@gmail.com>
2026-06-25 11:31:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Cole aqui as credenciais do seu projeto Supabase (Project Settings > API)
 // Project URL -> SUPABASE_URL
 // anon public key -> SUPABASE_ANON_KEY
-window.SUPABASE_URL = "COLE_AQUI_A_PROJECT_URL";
-window.SUPABASE_ANON_KEY = "COLE_AQUI_A_ANON_PUBLIC_KEY";
+window.SUPABASE_URL = "https://ynpinnobhmsottsmglfc.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlucGlubm9iaG1zb3R0c21nbGZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzNzcyMzQsImV4cCI6MjA5Nzk1MzIzNH0.C2AbGBxoZS11X1S9D899nopiaxuwoZQQyCi1kvQGOksY";
